8347033da2e547aa1b5c3b567bf1fb2c33a39d11
Gregory Brunner <gbrunner@esri.com>
2020-12-31 09:22:07 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,30 @@
 require([
-        "esri/Map",
-        "esri/views/MapView",
-        "esri/layers/Layer"
-      ], function (Map, MapView, Layer) {
-        var map = new Map({
-          basemap: "gray-vector"
-        });
+      "esri/views/MapView",
+      "esri/WebMap",
+      "dojo/domReady!"
+    ], function(
+      MapView, WebMap
+    ) {
 
-        var view = new MapView({
-          map: map,
-          container: "viewDiv",
-          zoom: 7,
-          center: [-90, 40]
-        });
-
-        Layer.fromPortalItem({
-          portalItem: {
-            // autocasts as new PortalItem()
-            /*id: "c0f09a71805f4d45a6b6f3f1e0bdc7fc"*/
-            /*id: "af1ad38816814b7eba3fe74a3b84412d"*/
-            id: "c0238b8ae811443dbe52437496a1a514"
-          }
-        })
-          .then(addLayer)
-          .catch(rejection);
-
-        // Adds the layer to the map once it loads
-        function addLayer(layer) {
-          map.add(layer);
-        }
-
-        function rejection(error) {
-          console.log("Layer failed to load: ", error);
+      /************************************************************
+       * Creates a new WebMap instance. A WebMap must reference
+       * a PortalItem ID that represents a WebMap saved to
+       * arcgis.com or an on-premise portal.
+       *
+       * To load a WebMap from an on-premise portal, set the portal
+       * url with esriConfig.portalUrl.
+       ************************************************************/
+      var webmap = new WebMap({
+        portalItem: { // autocasts as new PortalItem()
+          id: "b43f6b54917e4b2f953b27f4728b72db"
         }
       });
+
+      /************************************************************
+       * Set the WebMap instance to the map property in a MapView.
+       ************************************************************/
+      var view = new MapView({
+        map: webmap,
+        container: "viewDiv"
+      });
+    });
